c59fbe16457a7221670ef70b867da7878e6d1a3b
Enrique Gomez <gomez.enrique4595@gmail.com>
2022-02-15 21:54:20 -0800

    Added Ch2 Excercises

---
+++ Ch2Excercises/personal_info.py
@@ -0,0 +1,15 @@
+#Display name
+#Display address, city, state, and zip
+#Display telephone number
+#Display college major
+
+name = "Enrique Gomez" 
+address = "12344 Waverly Drive, Houston, TX, 91402"
+telephone_number = "123-233-4567"
+major = "computer science"
+
+print(name)
+print(address)
+print(telephone_number)
+print(major)
+
