e01b826e50fd52229718e97c4147a2a98ddba3e9
Enrique Gomez <gomez.enrique4595@gmail.com>
2022-02-16 19:45:08 -0800

    format modified

--- Ch2Excercises/sales_prediction.py
+++ Ch2Excercises/sales_prediction.py
@@ -6,4 +6,4 @@ total_sales = float(input("Enter the total amount of sales: "))
 PERCENT_SALES = 0.23
 annual_profit = total_sales * PERCENT_SALES
 
-print("Total sales $",format(annual_profit, ",.2f"))
+print("Total sales $",format(annual_profit, ",.2f"), sep='')
