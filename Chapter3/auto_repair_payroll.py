365b68bf3e1a5aa7dbc945844bc4666b16817ced
Enrique Gomez <gomez.enrique4595@gmail.com>
2022-02-21 15:29:11 -0800

    started payroll program

---
+++ Chapter3/auto_repair_payroll.py
@@ -0,0 +1,18 @@
+#Get the number of hours worked
+#Get the hourly pay rate.
+#If the employee worked more than 40 hours:
+#   Calculate and display the gross pay with overtime. 
+#Else:
+#   Calculate and display the gross pay as usual. 
+
+STANDARD_HOURS = 40 
+OVERTIME_PAYRATE = 1.5 
+
+hours_worked = float(input('Enter the amount of hours worked: ')) 
+hourly_payrate = float(input('Enter your hourly pay rate $: '))
+
+if hours_worked > STANDARD_HOURS:
+    
+else: 
+    #display gross pay as usual
+
