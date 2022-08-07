76d73f0b6ddf1e4c2d87985fcb7fa084dd6b98f3
gomezenrique93 <gomez.enrique4595@gmail.com>
2022-08-06 18:44:52 -0700

    added distance traveled

---
+++ Ch2Exercises/distance_traveled.py
@@ -0,0 +1,14 @@
+#distance formula: distance = speed x time
+
+SPEED = 70
+time1 = 6
+time2 = 10
+time3 = 15
+
+distance1 = SPEED * time1
+distance2 = SPEED * time2
+distance3 = SPEED * time3   
+
+print('In ', time1, ' hours the car will travel ', distance1, ' miles')
+print('In ', time2, ' hours the car will travel ', distance2, ' miles')
+print('In ', time3, ' hours the car will travel ', distance3, ' miles')
