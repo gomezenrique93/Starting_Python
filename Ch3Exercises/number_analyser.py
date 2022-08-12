8bbe737d6ce7608f0b3cdd106900ec67b5c10d31
gomezenrique93 <gomez.enrique4595@gmail.com>
2022-08-11 19:34:29 -0700

    added Ch3Exercises

---
+++ Ch3Exercises/number_analyser.py
@@ -0,0 +1,22 @@
+#ask the user for an integer
+#if number is greater than 0 print "Positive"
+#if number is less than 0 print "Negative"
+#if number is equal to 0 print "Zero"
+#if number is even display "Even"
+#if number is odd display "Odd"
+
+user_number = input("Enter an integer ")
+
+user_number = int(user_number)
+
+if user_number < 0:
+    print("Negative")
+if user_number > 0:
+    print("Positive")
+if user_number == 0:
+    print("Zero")
+if user_number % 2 == 0:
+    print("Is Even")
+if user_number % 3 == 0:
+    print("Is Odd")
+
