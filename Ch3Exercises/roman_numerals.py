6c1b7770ef5619f8474bdc9d4e55f0e62f11355b
gomezenrique93 <gomez.enrique4595@gmail.com>
2022-11-19 23:26:02 -0800

    added roman_numerals.py

---
+++ Ch3Exercises/roman_numerals.py
@@ -0,0 +1,28 @@
+#Prompt the user to enter a number between 1 and 10
+#Output the appropriate Roman Numeral
+
+user_number = int(input("Enter a number: "))
+
+if user_number < 1 or user_number > 10:
+    print("Enter a number between 1 and 10")
+elif user_number == 1:
+    print("Roman Numeral: I")
+elif user_number == 2:
+    print("Roman Numberal: II")
+elif user_number == 3:
+    print("Roman Numeral: III")
+elif user_number == 4:
+    print("Roman Numeral: IV")
+elif user_number == 5:
+    print("Roman Numeral: V")
+elif user_number == 6:
+    print("Roman Numeral: VI")
+elif user_number ==7:
+    print("Roman Numeral: VII")
+elif user_number == 8:
+    print("Roman Numeral: VIII")
+elif user_number == 9:
+    print("Roman Numeral: IX")
+elif user_number == 10:
+    print("Roman Numeral: X")
+
