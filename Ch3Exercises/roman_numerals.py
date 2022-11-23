a4ea89c2b3bcc8aad26ee5292f598fcea3099c58
Gato <gato@pop-os.localdomain>
2022-11-22 19:35:14 -0800

    rearranged control structure

--- Ch3Exercises/roman_numerals.py
+++ Ch3Exercises/roman_numerals.py
@@ -4,25 +4,26 @@
 user_number = int(input("Enter a number: "))
 
 if user_number < 1 or user_number > 10:
-    print("Enter a number between 1 and 10")
-elif user_number == 1:
-    print("Roman Numeral: I")
-elif user_number == 2:
-    print("Roman Numberal: II")
-elif user_number == 3:
-    print("Roman Numeral: III")
-elif user_number == 4:
-    print("Roman Numeral: IV")
-elif user_number == 5:
-    print("Roman Numeral: V")
-elif user_number == 6:
-    print("Roman Numeral: VI")
-elif user_number ==7:
-    print("Roman Numeral: VII")
-elif user_number == 8:
-    print("Roman Numeral: VIII")
-elif user_number == 9:
-    print("Roman Numeral: IX")
-elif user_number == 10:
-    print("Roman Numeral: X")
+    print("ERROR, Enter a number between 1 and 10")
+else:
+    if user_number == 1:
+        print("Roman Numeral: I")
+    elif user_number == 2:
+        print("Roman Numberal: II")
+    elif user_number == 3:
+        print("Roman Numeral: III")
+    elif user_number == 4:
+        print("Roman Numeral: IV")
+    elif user_number == 5:
+        print("Roman Numeral: V")
+    elif user_number == 6:
+        print("Roman Numeral: VI")
+    elif user_number ==7:
+        print("Roman Numeral: VII")
+    elif user_number == 8:
+        print("Roman Numeral: VIII")
+    elif user_number == 9:
+        print("Roman Numeral: IX")
+    elif user_number == 10:
+        print("Roman Numeral: X")
 
