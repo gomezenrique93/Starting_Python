4a430bbc24e5399d10f565ec64513d11f11ee0e3
Gato <gomez.enrique4595@gmail.com>
2022-12-06 21:50:06 -0800

    added calories_burned.py

---
+++ Ch4Exercises/calories_burned.py
@@ -0,0 +1,12 @@
+# This program will show you how many calories you've burned
+
+CALORIES_PER_MINUTE = 4.2 
+
+# Starting interval time.
+START_TIME = 10
+
+# Ending interval time.
+END_TIME = 30
+
+for minutes in range(START_TIME, END_TIME):
+    
