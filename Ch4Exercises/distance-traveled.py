444c735dbdb79ab4733283d9676dfa0815f6dc90
Gato <gomez.enrique4595@gmail.com>
2023-01-19 17:25:11 -0800

    added distance-traveled.py

--- Ch4Exercises/distance-traveled.py
+++ Ch4Exercises/distance-traveled.py
@@ -2,5 +2,19 @@
 #It should then ask the user how many hours they've travelled
 #Use a loop to display the distance the vehicle has travelled
 
-speed = int(input("What is the speed of the vehicle in mph?" ))
-print(speed)
+#Prompt user for speed of the vehicle
+speed = int(input("What is the speed of the vehicle in mph? "))
+
+#Prompt the user for the travel time
+hours = int(input("How many hours has it traveled? "))
+
+#Initialize accumulator to store distance
+distance = 0
+
+print("Hour\tDistance Travelled")
+for time in range(hours):
+    #Algorithm to calculate distance
+    distance = speed * (time + 1)
+
+    #Will print distance and time for variable
+    print(time+1,"\t",distance)
