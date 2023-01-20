e3942a2ea71d2dda640bcef327e9ec6a7e4e9222
Gato <gomez.enrique4595@gmail.com>
2023-01-19 16:43:25 -0800

    added distance-traveled.py

---
+++ Ch4Exercises/distance-traveled.py
@@ -0,0 +1,6 @@
+#This program will ask the user for the speed of a vehicle(in miles per hour)
+#It should then ask the user how many hours they've travelled
+#Use a loop to display the distance the vehicle has travelled
+
+speed = int(input("What is the speed of the vehicle in mph?" ))
+print(speed)
