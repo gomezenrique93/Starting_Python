c59710cfd18c46a018a7b1e7df5427dd615f0805
Gato <gomez.enrique4595@gmail.com>
2023-05-20 13:33:26 -0700

    formatted output

--- Ch4Exercises/average_rainfall.py
+++ Ch4Exercises/average_rainfall.py
@@ -3,10 +3,13 @@
 # Loop for number of years
 # Inner loop will loop for 12 times
 
+# Prompts user to enter in number of years for rainfall
 number_of_years = int(input("How many years would you like to calculate rainfall? "))
 
+# Variable to store total rainfall per year and month
 total_rainfall = 0
 
+# Counter variable to count the total number of months
 total_months = 0
 
 for i in range(number_of_years):
@@ -23,7 +26,12 @@ for i in range(number_of_years):
         # This will keep track of the total number of months
         total_months += 1
 
-# Calculation Section for average
+# Calculation for average rainfall
 average_rainfall = total_rainfall / total_months
 
-print(average_rainfall)
+# Displays total rainfall per year
+print("Total rainfall per year is", format(total_rainfall, ".2f"), " inches.")
+
+# Displays average rainfall 
+print("Average rainfall is ", format(average_rainfall, ".2f"), " inches.")
+
