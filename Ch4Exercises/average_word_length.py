b3f3384d0a9c26febb7632e452fc6349a8a32c52
Gato <gomez.enrique4595@gmail.com>
2023-05-21 21:03:41 -0700

    commented average_world_length.py

--- Ch4Exercises/average_word_length.py
+++ Ch4Exercises/average_word_length.py
@@ -5,22 +5,37 @@
 # Will hold the total characters in a word
 total = 0
 
+# Holds default value to run loop
 truthy = True
 
+# Records total input
 total_inputs = 0
 
+# Will hold average
 average = 0
+
 # Will make sure that the user does not enter a space
 while truthy == True:
+
+    #Prompts user for the word
     user_word = input("Please enter a word:")
+
+    # Will hold the length of the word
     word_length = len(user_word)
+
+    # Will hold total word length
     total += word_length
+
+    # Will record total length
     total_inputs += 1
+
+    # Will check for a space
     if user_word == " ":
         truthy = False
         total_inputs -= 1
 
-    average = total / total_inputs
-print(total_inputs)
+    # Calculates the average
+    average = total // total_inputs
+
 print(average)
    
