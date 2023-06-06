affa579123dcce61332348243cdfe163071443a3
Gato <gomez.enrique4595@gmail.com>
2023-06-06 12:07:26 -0700

    added number_analysis.py

---
+++ Ch7Exercises/number_analysis.py
@@ -0,0 +1,25 @@
+def main():
+    values = collect_values()
+    print(values)
+
+def collect_values():
+
+    # Empty list
+    user_values = []
+
+    # Prompts user for value 20 times
+    for i in range(20):
+        # Collect values from user
+        user_values = int(input("Collect user values: "))
+
+    return user_values
+    
+# function to find the lowest number
+
+# function to find the highest number
+
+# The total numbers in a list
+
+# The average numbers of the list 
+
+main()
