4dc401da64e4f89529635338691afc05843c241e
Gato <gomez.enrique4595@gmail.com>
2023-06-08 12:35:38 -0700

    finished number_analysis.py

--- Ch7Exercises/number_analysis.py
+++ Ch7Exercises/number_analysis.py
@@ -1,6 +1,22 @@
 def main():
+    # values holds the 
+    # list from collect_values() 
     values = collect_values()
-    print(values)
+
+    # lowest_value holds the lowest
+    # value from the list
+    lowest_value = find_lowest(values)
+    print("Lowest value is: ", lowest_value)
+
+    ## maximum_value holds the lowest
+    # value from the list
+    maximum_value = find_highest(values)
+    print("Maximum value is: ", maximum_value)
+
+    calculated_total = calculate_total(values)
+
+    calculated_average = calculate_average(calculated_total, len(values))
+    print(calculated_average)
 
 def collect_values():
     # Empty list
@@ -14,15 +30,48 @@ def collect_values():
         # Adds values to the 
         # user_vallues list
         user_values.append(val)
-
+    
+    # returns list of user values
     return user_values
 
 # function to find the lowest number
+def find_lowest(val):
+    
+    # Variable to hold minimum value
+    minimum_value = min(val)
+
+    # Returns the smallest value
+    return minimum_value
 
 # function to find the highest number
+def find_highest(val):
 
-# The total numbers in a list
+    # Variable to hold max value
+    maximum_value = max(val)
 
-# The average numbers of the list 
+    # Returns the maximum value
+    return maximum_value
+
+# Calculates the total numbers in a list
+def calculate_total(val):
+
+    # Accumulator variable
+    total = 0
+
+    # Loop to calculate total
+    for i in range(len(val)):
+        total += val[i]
+
+    # returns running total
+    return total
+
+# Calculates the average numbers of the list 
+def calculate_average(total, length):
+
+    # Average calculation
+    average = total / length 
+
+    # Returns average value
+    return average
 
 main()
