f5b2ee0d11f1c5cb44c754940510f3f45600c06c
Gato <gato@pop-os.localdomain>
2023-07-04 22:52:37 -0700

    added empty_window1-3.py

---
+++ Chapter13/empty_window1.py
@@ -0,0 +1,14 @@
+#   This program displays an empty window.
+
+import tkinter
+
+def main():
+    # Create the amin window widget.
+    main_window = tkinter.Tk()
+
+    #   Enter the tkinter main loop.
+    tkinter.mainloop()
+
+# Call the main function.
+main()
+
